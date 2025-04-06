3a1ac4f75b20b33333de8ffbe30093a97e117b6c
dvshmkr <dave@gvn.com>
2025-04-06 22:59:27 +0000

    edit on configure_software.sh

--- CodeDeploy/configure_software.sh
+++ CodeDeploy/configure_software.sh
@@ -17,31 +17,18 @@ export DATABASE_DB_NAME=TEST-routes
 export DATABASE_USER=web_user
 
 echo "check 2: " >> /webapps/test.log
+echo "DATABASE_ROOT_PASSWORD: $DATABASE_ROOT_PASSWORD" >> /webapps/test.log
+echo "DATABASE_HOST: $DATABASE_HOST" >> /webapps/test.log
 
 # setup sql database
 cat /webapps/app/CodeDeploy/TEST-CreateDrop.sql | mysql -h $DATABASE_HOST -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD
 sed "s/SED_REPLACE_PASS/$DATABASE_PASSWORD/g" < /webapps/app/CodeDeploy/create_schema.sql | mysql -h $DATABASE_HOST -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD $DATABASE_DB_NAME
 /webapps/app/CodeDeploy/database_populate.py
 
-echo "check 3: " >> /webapps/test.log
-
-# copy in the nginx config
-mv -f /webapps/app/CodeDeploy/nginx.conf /etc/nginx/nginx.conf
-systemctl restart nginx
-
-# configure log file for uwsgi
-mkdir /var/log/uwsgi
-chown nginx:root /var/log/uwsgi
-systemctl restart uwsgi
-
-echo "check 4: " >> /webapps/test.log
-
 # push configuration into app.ini
 sed -i s/SED_REPLACE_DATABASE_HOST/$DATABASE_HOST/g /webapps/app/CodeDeploy/app.ini
 sed -i s/SED_REPLACE_DATABASE_DB_NAME/$DATABASE_DB_NAME/g /webapps/app/CodeDeploy/app.ini
 echo "env = ENV_PREFIX=TEST-" >> /webapps/app/CodeDeploy/app.ini
-
-echo "check 5: " >> /webapps/test.log
 
 # configure region for the app
 #EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
@@ -50,15 +37,32 @@ EC2_AVAIL_ZONE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.16
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 echo "env = AWS_DEFAULT_REGION=$EC2_REGION" >> /webapps/app/CodeDeploy/app.ini
 
-echo "check 6: " >> /webapps/test.log
+echo "check 3: " >> /webapps/test.log
+
+# set file permissions
+
+chown -R nginx:root /webapps/app/FlaskApp
+chown -R nginx:root /webapps/app/CodeDeploy
+
+
+# copy in the nginx config
+mv -f /webapps/app/CodeDeploy/nginx.conf /etc/nginx/nginx.conf
+systemctl restart nginx
+
+# configure log file for uwsgi
+mkdir /var/log/uwsgi
+chown nginx:nginx /var/log/uwsgi
+systemctl restart uwsgi
+
+echo "check 4: " >> /webapps/test.log
 
 # display the deployment group in the footer
 echo $DEPLOYMENT_GROUP_NAME > /webapps/app/FlaskApp/templates/buildinfo.html
 
-echo "check 7: " >> /webapps/test.log
+echo "check 5: " >> /webapps/test.log
 
 # configure upstart to run uwsgi
 #mv -f /webapps/app/CodeDeploy/uwsgi.conf /etc/init/uwsgi.conf
-mv -f /webapps/app/CodeDeploy/app.ini /webapps/app/FlaskApp/
+#mv -f /webapps/app/CodeDeploy/app.ini /webapps/app/FlaskApp/
 
-echo "check 8: " >> /webapps/test.log
+echo "check 6: " >> /webapps/test.log
