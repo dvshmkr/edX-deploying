d2d38ab20a9eae987cf481a50911184c4a01dcb7
dvshmkr <dave@gvn.com>
2025-04-07 23:38:20 +0000

    fixing db permissions

--- CodeDeploy/create_schema.sql
+++ CodeDeploy/create_schema.sql
@@ -22,7 +22,11 @@ direct_miles INT,
 total_miles INT
 );
 
-CREATE USER 'web_user'@'%' IDENTIFIED BY 'SED_REPLACE_PASS';
+CREATE USER IF NOT EXISTS 'web_user'@'%' IDENTIFIED BY 'SED_REPLACE_PASS';
+
+-- Grant USAGE privilege on the TEST-routes database
+GRANT USAGE ON `TEST-routes`.* TO 'web_user'@'%';
+-- Grant SELECT privileges on specific tables within TEST-routes
 GRANT SELECT ON `TEST-routes`.airport TO 'web_user'@'%';
 GRANT SELECT ON `TEST-routes`.route_miles TO 'web_user'@'%';
 
