c5d7f763d7fe252c9dcff4c25cc714222a12162f
dvshmkr <dave@gvn.com>
2025-04-08 01:20:09 +0000

    shorted db connections string in db_test.py

--- FlaskApp/db_test.py
+++ FlaskApp/db_test.py
@@ -3,7 +3,9 @@ import mysql.connector
 
 def get_database_connection():
     "Build a database connection"
-    conn = mysql.connector.connect(user='root', password='1keytone',host='test-stack-labdbinstance-iwn8wkt1epiz.cvasewix0fhp.us-west-2.rds.amazonaws.com', database='TEST-routes')
+    conn = mysql.connector.connect(user='root', password='1keytone',
+        host='test-stack-labdbinstance-iwn8wkt1epiz.cvasewix0fhp.us-west-2.rds.amazonaws.com',
+        database='TEST-routes')
     return conn
     
 mydb = get_database_connection()
